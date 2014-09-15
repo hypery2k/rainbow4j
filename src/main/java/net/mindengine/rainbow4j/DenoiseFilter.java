37ba55b55623a664bea36c610685e97be755cdf4
Ivan Shubin <ivan.ishubin@gmail.com>
2014-09-15 15:50:52 +0200

    Moved filters to main

--- src/main/java/net/mindengine/rainbow4j/DenoiseFilter.java
+++ src/main/java/net/mindengine/rainbow4j/DenoiseFilter.java
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 ******************************************************************************/
-package net.mindengine.rainbow4j.tests;
+package net.mindengine.rainbow4j;
 
 import net.mindengine.rainbow4j.ImageFilter;
 
